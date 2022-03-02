d7aecc053c19dc28cc0dbe4730ce864ea4f65b56
Nyior <nyior.clement@aun.edu.ng>
2022-03-02 18:33:45 +0100

    render question and options to the index page

--- components/Choice/choice.tsx
+++ components/Choice/choice.tsx
@@ -3,8 +3,15 @@ import styles from "./Choice.module.scss"
 export const Choice = props =>  {
   return (
 
-    <div className={styles.card}>
+    <div className={styles.questionCard}>
+      <div className={styles.content}>
+        <div>
+          <input type="radio" className={styles.input}/>
+          {/* <span className={styles.customRadioButton}></span> */}
+        </div>
+        
         <p>{props.text}</p>
+      </div>
     </div>
 
   )
